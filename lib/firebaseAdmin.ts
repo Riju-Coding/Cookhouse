0865d728bfafd9dc356f6f67b14d0ff4cf57b245
RijCode <nathsudiptaaa123@gmail.com>
2026-04-10 16:08:44 +0530

    redeploy after env fix

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,12 +1,14 @@
 import * as admin from "firebase-admin"
 
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+}
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    } as admin.ServiceAccount),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   })
 }
 
